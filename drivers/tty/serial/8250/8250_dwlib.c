981059a6637c6fe8cd405313af764758fc8ea0ca
Samuel Holland <samuel@sholland.org>
2022-07-18 23:07:30 -0500

    serial: 8250_dw: Use CPR value from pdata when autocfg is skipped
    Variants can provide a CPR value in platform data. Use this value to set
    the appropriate port capabilities even when autocfg is not possible.
    
    Signed-off-by: Samuel Holland <samuel@sholland.org>

--- drivers/tty/serial/8250/8250_dwlib.c
+++ drivers/tty/serial/8250/8250_dwlib.c
@@ -195,6 +195,14 @@ void dw8250_setup_port(struct uart_port *p)
 	if (!reg)
 		return;
 
+	dw8250_setup_cpr(p, reg);
+}
+EXPORT_SYMBOL_GPL(dw8250_setup_port);
+
+void dw8250_setup_cpr(struct uart_port *p, u32 reg)
+{
+	struct uart_8250_port *up = up_to_u8250p(p);
+
 	/* Select the type based on FIFO */
 	if (reg & DW_UART_CPR_FIFO_MODE) {
 		p->type = PORT_16550A;
@@ -209,4 +217,4 @@ void dw8250_setup_port(struct uart_port *p)
 	if (reg & DW_UART_CPR_SIR_MODE)
 		up->capabilities |= UART_CAP_IRDA;
 }
-EXPORT_SYMBOL_GPL(dw8250_setup_port);
+EXPORT_SYMBOL_GPL(dw8250_setup_cpr);
